83cf221b74d0994c94f7954d575558b892f9767e
Faysal <faysal9.2014@gmail.com>
2024-02-16 19:18:53 +0600

    first commit

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -2,9 +2,6 @@ import random
 import string
 
 chars = string.ascii_letters+string.digits+string.punctuation
-# password = ""
 size = 12
-# for i in range(size):
-#     password += random.choice(chars)
 password = "".join([random.choice(chars) for i in range(size)])
 print("Your random passowrd is :",password)
